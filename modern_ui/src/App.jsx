d7ceb4e73ad80f4f2d708845bb676f3f135a20f5
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2022-09-30 06:48:17 -0700

    first commit

---
+++ modern_ui/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <div>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default App
